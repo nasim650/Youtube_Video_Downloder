60405c605430dc707972bdbf1645c849409ed8ee
AmiSezan <smmojahedulislam@gmail.com>
2024-05-18 23:36:37 +0600

    Removing Print Check adding image & title

--- gui.py
+++ gui.py
@@ -1,3 +1,21 @@
 from tkinter import *
 from pytube import YouTube
 from moviepy.editor import VideoFileClip
+root = Tk()
+
+root.title('Youtube Video Downloader')
+root.geometry('450x550+400+50')
+root.configure(bg='#cdc9c8')
+
+root.resizable(False,False)
+
+#for image
+img = PhotoImage(file='logo.png')
+Label(image =img,bd=1,bg='#cdc9c8').place(x=70,y=20)
+
+
+
+root.mainloop()
+
+
+
